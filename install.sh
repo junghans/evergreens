dbd88eb42300c73bd80b034b325b49aadaa3b24f
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2008-10-06 13:50:16 +0200

    install.sh: added source_file support

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@
 
 #version 0.1  06.10.08 -- initial version
 #version 0.2  06.10.08 -- added --testing option
+#version 0.3  06.10.08 -- added source_file support
 
 usage="Usage: ${0##*/} WHERE"
 opts="-v -f"
 cmd="ln -s"
 echo=""
+quiet="no"
+source_file="README"
 
 help () {
   cat << eof
@@ -26,7 +29,9 @@ OPTIONS:
     --hg            Show last log message for hg (or cvs)
 
 This will produce global links, to produce local links use
-install_scripts (from the evergreen repo).
+install_scripts (from the evergreen repo). The files to
+install can also be determined by the FILES_TO_INSTALL
+section in $source_file.
 
 Examples:  ${0##*/} -ic cp $HOME/bin
            ${0##*/} \$HOME
@@ -53,6 +58,7 @@ while [ "${1#-}" != "$1" ]; do
     shift ;;
    -q | --quiet)
     opts=${opts/-v/}
+    quiet="yes"
     shift ;;
    -c | --command)
     [[ -n "$(type -p "$2")" ]] || { echo Command \"$2\" not found; exit 1; }
@@ -82,7 +88,24 @@ fi
 
 [[ -d "$1" ]] || { echo Dir \"$1\" not found; exit 1; }
 aim=$1
-
 thisdir=$PWD
 cd $aim
-find -L $thisdir -type f -perm "-u=xr" \! -name ${0##*/} \! -name "*~" -exec $echo $cmd $opts {} . \;
+
+if [ -f "$thisdir/$source_file" ]; then
+   filelist=$(sed -n '/FILES_TO_INSTALL/,/END_FILES_TO_INSTALL/p' "$thisdir/$source_file" | sed "/^#/d;1d;\$d;s#^#$thisdir/#")
+else
+   filelist=$(find -L $thisdir -type f -perm "-u=xr" \! -name "*~")
+fi
+
+if [ "$quiet" = "no" ]; then
+   echo -n "Files to install: "
+   for myfile in $filelist; do
+       echo -n "${myfile##*/} "
+   done
+   echo
+   echo Where to install: $thisdir
+fi
+
+for myfile in $filelist; do
+   $echo $cmd $opts $myfile .
+done
